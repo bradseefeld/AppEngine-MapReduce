218c05eb1d884e66297be3967e59190f4051be95
Brad Seefeld <bradley.seefeld@gmail.com>
2011-05-29 13:54:10 -0700

    Finished a makeshift configuration for getting everything running

---
+++ launch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ant
+mvn install:install-file -DgroupId=com.google.appengine.tools -DartifactId=tools-mapreduce -Dversion=1.0.0-SNAPSHOT -Dpackaging=jar -Dfile=dist/lib/appengine-mapper.jar
+mvn gae:run -DargLine=-Xmx2048m
